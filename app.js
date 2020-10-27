d4b762763f1388830b27d890b7c8e63ea1dedd7d
nnewnew <godsnew2542@gmail.com>
2020-10-28 02:00:42 +0700

    update data

--- app.js
+++ app.js
@@ -1,109 +1,84 @@
-function Clear() {
-  document.getElementById("answer").innerHTML = "";
+function Clear(ans) {
+    console.log("clear");
+    document.getElementById("answer").innerHTML = '';
 }
 
-function Add() {
-  var num1 = document.getElementById("num1").value;
-  var num2 = document.getElementById("num2").value;
-  var Char1 = num1.charAt(0);
-  var Char2 = num2.charAt(0);
-  var float1 = parseFloat(num1);
-  var float2 = parseFloat(num2);
-  var sum = float1 + float2;
+function calculator(num1, num2, option) {
+    var Char1 = num1.charAt(0);
+    var Char2 = num2.charAt(0);
+    var float1 = parseFloat(num1);
+    var float2 = parseFloat(num2);
 
-  if (num1 == "" || num2 == "") {
-    alert("กรุณากรอกข้อมูลให้ครบถ้วน");
-  } else if (Char1 == "-" || Char2 == "-") {
-    alert("กรุณากรอกข้อมูลเป็นจำนวนเต็มเท่านั้น");
-  } else {
-    $("#answer").append();
-    var Add = `<h2> = &emsp; ${sum}</h2>`;
-    $("#answer").append(Add);
-  }
+    if (num1 == "" || num2 == "") {
+        alert("กรุณากรอกข้อมูลให้ครบถ้วน");
+    } else if (Char1 == "-" || Char2 == "-") {
+        alert("กรุณากรอกข้อมูลเป็นจำนวนเต็มเท่านั้น");
+    } else {
+        if (option == "Add") {
+            add(float1, float2);
+        }
+        if (option == "Minus") {
+            minus(float1, float2);
+        }
+        if (option == "Multiply") {
+            multiply(float1, float2);
+        }
+        if (option == "Divide") {
+            if (Char2 == "0") {
+                alert("ไม่สามารถคำนวนหาค่าได้");
+            } else {
+                divide(float1, float2);
+            }
+        }
+        if (option == "Percent") {
+            if (Char2 == "0") {
+                alert("ไม่สามารถคำนวนหาค่าได้");
+            } else {
+                percent(float1, float2);
+            }
+        }
+    }
 }
 
-function Minus() {
-  var num1 = document.getElementById("num1").value;
-  var num2 = document.getElementById("num2").value;
-  var Char1 = num1.charAt(0);
-  var Char2 = num2.charAt(0);
-  var float1 = parseFloat(num1);
-  var float2 = parseFloat(num2);
-  var sum = float1 - float2;
-
-  if (num1 == "" || num2 == "") {
-    alert("กรุณากรอกข้อมูลให้ครบถ้วน");
-  } else if (Char1 == "-" || Char2 == "-") {
-    alert("กรุณากรอกข้อมูลเป็นจำนวนเต็มเท่านั้น");
-  } else {
-    $("#answer").append();
-    var Minus = `<h2> = &emsp; ${sum}</h2>`;
-    $("#answer").append(Minus);
-  }
+function clearOutput() {
+    document.getElementById("num1").value = ''
+    document.getElementById("num2").value = ''
 }
 
-function Multiply() {
-  var num1 = document.getElementById("num1").value;
-  var num2 = document.getElementById("num2").value;
-  var Char1 = num1.charAt(0);
-  var Char2 = num2.charAt(0);
-  var float1 = parseFloat(num1);
-  var float2 = parseFloat(num2);
-  var sum = float1 * float2;
-
-  if (num1 == "" || num2 == "") {
-    alert("กรุณากรอกข้อมูลให้ครบถ้วน");
-  } else if (Char1 == "-" || Char2 == "-") {
-    alert("กรุณากรอกข้อมูลเป็นจำนวนเต็มเท่านั้น");
-  } else {
+function updateDisplay(sum) {
+    clearOutput();
     $("#answer").append();
-    var Multiply = `<h2> = &emsp; ${sum}</h2>`;
-    $("#answer").append(Multiply);
-  }
+    var dis = `<h2> = &emsp; ${sum}</h2>`;
+    $("#answer").append(dis);
+
 }
 
-function Divide() {
-  var num1 = document.getElementById("num1").value;
-  var num2 = document.getElementById("num2").value;
-  var Char1 = num1.charAt(0);
-  var Char2 = num2.charAt(0);
-  var float1 = parseFloat(num1);
-  var float2 = parseFloat(num2);
-  var sum = float1 / float2;
-
-  if (num1 == "" || num2 == "") {
-    alert("กรุณากรอกข้อมูลให้ครบถ้วน");
-  } else if (Char1 == "-" || Char2 == "-") {
-    alert("กรุณากรอกข้อมูลเป็นจำนวนเต็มเท่านั้น");
-  } else if (Char2 == "0") {
-    alert("ไม่สามารถคำนวนหาค่าได้");
-  } else {
-    $("#answer").append();
-    var Divide = `<h2> = &emsp; ${sum}</h2>`;
-    $("#answer").append(Divide);
-  }
+function add(float1, float2) {
+    var sum = float1 + float2;
+    updateDisplay(sum);
 }
 
-function Percent() {
-  var num1 = document.getElementById("num1").value;
-  var num2 = document.getElementById("num2").value;
-  var Char1 = num1.charAt(0);
-  var Char2 = num2.charAt(0);
-  var float1 = parseFloat(num1);
-  var float2 = parseFloat(num2);
-  var sum = (float1 / float2) * 100;
+function minus(float1, float2) {
+    var sum = float1 - float2;
+    updateDisplay(sum);
+}
 
-  if (num1 == "" || num2 == "") {
-    alert("กรุณากรอกข้อมูลให้ครบถ้วน");
-  } else if (Char1 == "-" || Char2 == "-") {
-    alert("กรุณากรอกข้อมูลเป็นจำนวนเต็มเท่านั้น");
-  } else if (Char2 == "0") {
-    alert("ไม่สามารถคำนวนหาค่าได้");
-  } else if (sum > "100") {
-    alert("ไม่สามารถคำนวนหาเปอร์เซ็นได้");
-  } else {
-    $("#answer").append();
-    var Percent = `<h2> = &emsp; ${sum} %</h2>`;
-    $("#answer").append(Percent);
-  }
+function multiply(float1, float2) {
+    var sum = float1 * float2;
+    updateDisplay(sum);
+}
+
+function divide(float1, float2) {
+    var sum = float1 / float2
+    updateDisplay(sum);
+}
+
+function percent(float1, float2) {
+    var sum = (float1 / float2) * 100;
+
+    if (sum > "100") {
+        alert("ไม่สามารถคำนวนหาเปอร์เซ็นได้");
+    } else {
+        updateDisplay(sum);
+    }
 }
